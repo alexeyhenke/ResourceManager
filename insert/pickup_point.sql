3e3f8e52fd9e97f21f1325bba7ec1b56e997aafc
Alexey <alexeyhenke@yandex.ru>
2020-10-02 00:17:05 +0300

    data fix

--- insert/pickup_point.sql
+++ insert/pickup_point.sql
@@ -3,7 +3,7 @@
 USE RM;
 --
 --
-INSERT INTO `pickup_point` (`id`, `name`, `address`, `created_at`, `updated_at`) VALUES (0, 'Центральный', '191167, Санкт-Петербург площадь Александра Невского, 2-Д', '2020-01-08 16:43:31', '2020-01-08 16:43:31');
+INSERT INTO `pickup_point` (`id`, `name`, `address`, `created_at`, `updated_at`) VALUES (5, 'Центральный', '191167, Санкт-Петербург площадь Александра Невского, 2-Д', '2020-01-08 16:43:31', '2020-01-08 16:43:31');
 INSERT INTO `pickup_point` (`id`, `name`, `address`, `created_at`, `updated_at`) VALUES (1, 'Северный', 'г. Санкт-Петербург, парк "Сосновка"', '2020-01-08 16:43:31', '2020-01-08 16:43:31');
 INSERT INTO `pickup_point` (`id`, `name`, `address`, `created_at`, `updated_at`) VALUES (2, 'Южный', 'г. Санкт-Петербург, Московский проспект 100', '2020-01-08 16:43:31', '2020-01-08 16:43:31');
 INSERT INTO `pickup_point` (`id`, `name`, `address`, `created_at`, `updated_at`) VALUES (3, 'Западный', 'г. Санкт-Петербург, Васильевский остров ЛенЭКСПО павильон 3', '2020-01-08 16:43:31', '2020-01-08 16:43:31');
